64ac268bacda0061d7c747fc0a130a8db610fe45
vomkaraxioned <omkarv@axioned.com>
2022-08-07 18:58:21 +0530

    js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 /* Author: 
 
 */
+let items = document.getElementsByClassName("accordion-item");
+for (x in items)
+    items[x].addEventListener("click",showDescription);
 
-
+function showDescription() {
+    let para = document.getElementsByClassName("description");
+    alert(this.childNodes[3].style.display = 'block');
+}
 
 
 
